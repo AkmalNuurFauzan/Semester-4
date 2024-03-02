5b5bc7194e4acd7496536a54b06c0bc7c9027626
AkmalNuurFauzan <120092353+AkmalNuurFauzan@users.noreply.github.com>
2024-03-02 12:43:48 +0700

    Update Handphone.java

--- Pemrograman Berbasis Objek/Sesi 2/Handphone.java
+++ Pemrograman Berbasis Objek/Sesi 2/Handphone.java
@@ -1,9 +1,9 @@
-/*
-
-Author
-Name    : Akmal Nuur Fauzan
-NIM     : 20220040060
-
+/**
+*
+* @Author
+* @Name    : Akmal Nuur Fauzan
+* @NIM     : 20220040060
+*
 */
 public class Handphone{
     String jenis_hp;
@@ -22,10 +22,10 @@ public class Handphone{
         return tahun_pembuatan;
     }
 
-    public void static main(String[] args){
+    public static void main(String[] args){
         Handphone hp = new Handphone();
         hp.setDataHP("VIVO", 2023);
         System.out.println(hp.getJenisHP());
-        System.out.println(hp.tahun_pembuatan());
+        System.out.println(hp.getTahunPembuatan());
     }
 }
