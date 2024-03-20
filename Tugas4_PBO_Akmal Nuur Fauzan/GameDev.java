6b8953b2f306f9c386495795d95e62f1328cdcf6
AkmalNuurFauzan <120092353+AkmalNuurFauzan@users.noreply.github.com>
2024-03-20 15:07:56 +0700

    Update GameDev.java

--- Tugas4_PBO_Akmal Nuur Fauzan/GameDev.java
+++ Tugas4_PBO_Akmal Nuur Fauzan/GameDev.java
@@ -1,11 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 /**
  *
- * @author akmal
+ * @author Akmal Nuur Fauzan (20220040060)
  */
 public class GameDev extends Game{
     protected String Developer;
